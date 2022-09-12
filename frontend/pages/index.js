25681385ac435b67794fe6e17278038d380d0aa1
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-09-12 22:21:47 +0530

    bg image added

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,19 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import bg from "../src/assets/bg-grid.png";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>CreatorFi</title>
-        <meta name="description" content="A DeFi plus DeSo platform for creators" />
+        <meta
+          name="description"
+          content="A DeFi plus DeSo platform for creators"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        
+        <div className={styles.grid}>
+        <Image src={bg}/>
+
+        </div>
       </main>
     </div>
-  )
+  );
 }
