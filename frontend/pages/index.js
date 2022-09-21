1f2f61aaea498825deca6ba5314cb2ae1d088e0b
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-09-21 22:11:11 +0530

    stackeed changes

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import image from "../src/assets/art.svg";
 import bg from "../src/assets/bg-grid.png";
 import bg2 from "../src/assets/grid-png.png";
 
@@ -15,17 +16,19 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-        <div className={styles.hero}>
-          {/* <Image src={bg} /> */}
+        <div className={styles.left}>
           <div>
             <h1 className={styles.censtereda}>CreatorFi</h1>
             <h3 className={styles.censtereda}>
               A DeSo platform with DeFi tools for creators
             </h3>
           </div>
-          
+        </div>
+        <div className={styles.right}>
+          <div>
+            {/* <Image src={image} /> */}
+          </div>
         </div>
       </main>
     </div>
