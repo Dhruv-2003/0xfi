09fd3667e699e3f9ec64299c02c23f30ca001f7c
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-09-21 23:17:49 +0530

    assets saved

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,50 @@
 import Layout from "../src/components/Layout";
 import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import Head from "next/head";
+
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+    <Head>
+        <title>Artist3</title>
+        <meta
+          name="description"
+          content="Mint NFT as a proof for your digital artwork in simple steps and
+              sell it in form of fractions directly on the platform to earn from
+              it. Token Holders earn a profit by holding the tokens for a peroid
+              of time"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiConfig>
+    </>
   );
 }
 
--- frontend/pages/dashboard.jsx
+++ frontend/pages/dashboard.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import styles from "../styles/Home.module.css";
 
 export default function dashboard() {
   return (
     <div className={styles.container}>
-        <h1>Creator Dashboard</h1>
+      <h1>Creator Dashboard</h1>
     </div>
-  )
+  );
 }
