2e8fcd74c39e098c2b945b6daa03d8bbf76238a4
dhruv <dhruvagarwal2017@gmail.com>
2022-09-21 20:32:33 +0530

    contracts layout created

---
+++ hardhat/contracts/fundsreciever.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+/// recieve the amount from the payee and basically the most safe contract , that doesnot allows other contract to call any other functions
+/// just keeps check of the balance of the creators
+/// creators can just withdraw for the balance they have , or other master contract can maybe control to the flow of the money
+/// the master contract can manage the money into the system
---
+++ hardhat/contracts/paymentRequests.sol
@@ -0,0 +1,6 @@
+/// creator can create requests of the amount of money , with name , description for the payment requests
+/// show all the payment options for the payee
+/// send money to the funds contract directly on transfer , with no hold
+/// can even create pay links for some other person , to recieve the money was an when they want to claim
+/// can pay in eth
+/// choose other currency options to be able to pay with them , support to be added
