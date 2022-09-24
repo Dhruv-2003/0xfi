d2bcb03d1ca5a8e79578b09d9839845742a6a5aa
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-09-24 11:58:23 +0530

    stacked changes

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,16 +4,20 @@ import Link from "next/link";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Image from "next/image";
 import logo from "../assets/logo.svg";
+import image from "../assets/img-bg.png";
 
 export default function Layout({ children }) {
   return (
     <>
       <nav className={styles.navbar}>
         <div className={styles.logo}>
-        <Link href="/">
-          <Image className={styles.logo} src={logo} />
-          
-        </Link>
+          <Link href="/">
+            {/* <Image className={styles.logo} src={logo} /> */}
+            <span className={styles.brand}>
+              {/* CreatorFi */}
+              <Image className={styles.logo} src={image} />
+            </span>
+          </Link>
         </div>
         <ul className={styles.navmenu}>
           <li className={styles.navlink}>
