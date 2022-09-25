e2049eefd06e8105c167d49d047084cbcf61eac4
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-09-25 14:34:11 +0530

    register

--- frontend/src/components/ReceivedPaymets.jsx
+++ frontend/src/components/ReceivedPaymets.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import styles from '../css/Component.module.css'
-export default function ReceivedPaymets() {
-  return (
-    <div className={styles.generate}>
-        
-    </div>
-  )
+export default function ReceivedPaymets(props) {
+    return (
+        <div className={styles.payments}>
+            <h2>
+                Kushagra Sarathe
+                {props.name}
+            </h2>
+
+            <h3> Amount: 10 MATIC
+                {props.amount}
+            </h3>
+            <h3>
+                <u>Note:</u> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+                eaque veritatis
+                {props.note}
+            </h3>
+        </div>
+    )
 }
