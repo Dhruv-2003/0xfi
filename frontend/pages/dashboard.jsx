f3506fc7aac2ad1564e3cf9396deecd11f8941a5
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-09-25 14:13:11 +0530

    dashboard

--- frontend/pages/dashboard.jsx
+++ frontend/pages/dashboard.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import Button from "../src/components/Button";
+import Generate from "../src/components/Generate";
+import ReceivedPaymets from "../src/components/ReceivedPaymets";
 import styles from "../styles/Home.module.css";
 
-export default function dashboard() {
+export default function dashboard(props) {
   return (
-    <div className={styles.container}>
-      <h1>Creator Dashboard</h1>
+    <div className={styles.page}>
+      <h1 className={styles.heading}>Welcome to Dashboard</h1>
+      <div className={styles.dashboard}>
+        <div className={styles.dashboard_section}>
+          <div className={styles.balance}>
+            <h1>Kushagra Sarathe</h1>
+            <h2>Total Balance: 5 MATIC{props.balance} </h2>
+            <Button title="Withdraw" />
+          </div>
+        </div>
+        <div className={styles.dashboard_section}>
+          <ReceivedPaymets />
+        </div>
+        <div className={styles.dashboard_section}>
+          <Generate />
+        </div>
+      </div>
     </div>
   );
 }
